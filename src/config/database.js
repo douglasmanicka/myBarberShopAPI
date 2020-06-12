5415705f447435a3bf081d1414e6348eeb4401df
douglas manicka <douglasmanicka@gmail.com>
2020-06-12 19:29:35 -0300

    fix database config/add initial user migration

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,12 @@
+require('dotenv/config');
+
 module.exports = {
   dialect: 'postgres',
   host: process.env.POSTGRES_HOST_DEV,
-  usarname: process.env.POSTGRES_USARNAME_DEV,
+  username: process.env.POSTGRES_USER_DEV,
   password: process.env.POSTGRES_PASSWORD_DEV,
   database: process.env.POSTGRES_DATABASE_DEV,
+  port: 5432,
   define: {
     timestamp: true,
     underscored: true,
