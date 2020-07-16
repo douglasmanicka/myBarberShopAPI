a09dfb642eab9e637f4a40ac36e6b805e0f295ff
douglas manicka <douglasmanicka@gmail.com>
2020-07-15 21:33:29 -0300

    feat - fix yarn.lock/add booking migration-model

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,9 @@ const { Sequelize } = require('sequelize');
 const databaseConfig = require('../config/database');
 const User = require('../app/models/User');
 const File = require('../app/models/File');
+const Booking = require('../app/models/Booking');
 
-const models = [User, File];
+const models = [User, File, Booking];
 
 class Database {
   constructor() {
