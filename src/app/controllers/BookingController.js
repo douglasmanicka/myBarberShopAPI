9141c40e83f6f359044e0d20bdc031efbc943c0e
douglas manicka <douglasmanicka@gmail.com>
2020-07-22 08:37:36 -0300

    feat -  list user bookings pagination

--- src/app/controllers/BookingController.js
+++ src/app/controllers/BookingController.js
@@ -6,10 +6,13 @@ const File = require('../models/File');
 
 class BookingController {
   async index(req, res) {
+    const { page = 1 } = req.query;
     const bookings = await Booking.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
       attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
